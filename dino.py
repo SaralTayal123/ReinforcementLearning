df03cf9ffbac577dac6a211419f6ad93caebdda7
Saral <thunderbird211@gmail.com>
2020-06-23 21:55:42 +0800

    Dino Ready to train

--- dino.py
+++ dino.py
@@ -3,10 +3,17 @@ import cv2 as cv2
 from mss import mss
 from PIL import Image, ImageEnhance
 import keyboard
+import pyautogui
 import time
 
-import sys
-np.set_printoptions(threshold=sys.maxsize)
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+import random
+from tqdm import tqdm
+from tensorflow.keras.models import model_from_json
 
 
 class Enviornment:
@@ -26,16 +33,19 @@ class Enviornment:
 
     def step(self, action):
         if action == 0:
-            pass
-            keyboard.press_and_release('space')
+            pyautogui.press('space')
+            # keyboard.press_and_release('space')
         if action == 1:
-            pass
-            keyboard.press_and_release('down')
+            pyautogui.press('down')
+            # keyboard.press_and_release('down')
+        if action == 2:
+            junk = 5
 
         screenshot = self.sct.grab(self.mon)
         img = np.array(screenshot)[:, :, 0]
-        state = self._processImg(img)
-        done = self._done(state)
+        processedImg = self._processImg(img)
+        state = self._imageBankHandler(processedImg)
+        done = self._done(processedImg)
         reward = self._getReward(done)
 
         return state, reward, done
@@ -50,7 +60,7 @@ class Enviornment:
         # img = ImageEnhance.Contrast(img).enhance(5)
         img = self._contrast(img)
         img = np.reshape(img, (76,384, 1))
-        return self._imageBankHandler(img)
+        return img
 
     def _contrast(self,pixvals):
         minval = 32 #np.percentile(pixvals, 2)
@@ -66,6 +76,8 @@ class Enviornment:
 
         bank = [] + self.imageBank #easy way to deep copy
         toReturn = [] + bank + img
+        toReturn = np.array(toReturn)
+        toReturn = np.reshape(toReturn, (76,384,self.imageBankLength))
 
         #handle image saving and trimming
         self.imageBank.pop(0)
@@ -75,46 +87,167 @@ class Enviornment:
 
     def _getReward(self,done):
         if done:
-            return -100
+            return -50
         else: 
+            return 1
             return time.time() - self.startTime
         
-    def _done(self,state):
-        img = np.array(state[0])
-
+    def _done(self,img):
+        img = np.array(img)
+        img  = img[30:50, 180:203, :]
+        # print(np.sum(img))
+           
         # cv2.imshow("image", img)
         # if cv2.waitKey(25) & 0xFF == ord('q'):
         #     cv2.destroyAllWindows()
 
-        sum = np.sum(img)
-        listToCheck = [(18, 137), (19, 152), (16, 165), (16,178),
-                       (17, 206), (16, 216), (17, 232), (17, 247)]
-        val = 0
-        for elem in listToCheck:
-            val += img[elem][0]
-        val = val/8 #avg
-        expectedVal = 0.048561151079136694
-        
+        # listToCheck = [(18, 137), (19, 152), (16, 165), (16, 178),
+        #                (17, 206), (16, 216), (17, 232), (17, 247)]              
+        # val = 0
+        # for elem in listToCheck:
+        #     val += img[elem][0]
+        # val = val/8  # avg
+        # expectedVal = 0.025179856115107917
+
+        val = np.sum(img)
+        expectedVal = 331.9352517985612
         # print("val: ", val)
         # print("Difference: ", np.absolute(val-expectedVal))
-        if np.absolute(val-expectedVal) > 0.0008: #seems to work well
+        if np.absolute(val-expectedVal) > 1: #seems to work well
             return False
         return True
 
-#TODO agent class
+
+class Agent:
+    def __init__(self):
+
+        self.model = Sequential([
+            Conv2D(32, (8,8), input_shape=(76, 384, 4),
+                   strides=(2,2), activation='relu'),
+            MaxPooling2D(pool_size=(5,5), strides=(2, 2)),
+            Conv2D(64, (4,4), activation='relu', strides=(1,1)),
+            MaxPooling2D(pool_size=(7, 7), strides=(3, 3)),
+            Conv2D(128, (1, 1), strides=(1,1), activation='relu'),
+            MaxPooling2D(pool_size=(3,3), strides=(3,3)),
+            Flatten(),
+            Dense(384, activation='relu'),
+            Dense(64, activation="relu", name="layer1"),
+            Dense(8, activation="relu", name="layer2"),
+            Dense(2, activation="linear", name="layer3"), #2 outputs
+        ])
+        self.model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.001))
+        # self.model.load_weights("modelwack3.h5")
+        self.memory = []
+        print(self.model.summary())
+        self.xTrain = []
+        self.yTrain = []
+
+
+    def predict(self, state):
+        stateConv = state
+        # stateConv = np.squeeze(state).reshape(1,-1)
+        qval = self.model.predict(np.reshape(stateConv, (1, 76, 384, 4)))
+        return qval
+
+    def act(self, state):
+        qval = self.predict(state)
+        prob = tf.nn.softmax(tf.math.divide((qval.flatten()), 0.7)) #0.7 is the temperature/exploration factor
+        # print(np.array(prob))
+        action = np.random.choice(range(2), p=np.array(prob))
+        return action
+
+    def remember(self, state, nextState, action, reward, done):
+        self.memory.append(np.array([state, nextState, action, reward, done]))
+
+    def learn(self):
+        self.batchSize = 64
+
+        if len(self.memory) > 100000:
+            self.memory = []
+            print("trimming memory")
+        if len(self.memory) < self.batchSize:
+            print("too little info")
+            return  # still need to learn, too little memory
+        batch = random.sample(self.memory, self.batchSize)
+        #check how much time random samples take too
+
+        self.learnBatch(batch)
+
+    def learnBatch(self, batch, alpha=0.9):
+        batch = np.array(batch)
+        actions = batch[:, 2].reshape(self.batchSize).tolist()
+        rewards = batch[:, 3].reshape(self.batchSize).tolist()
+
+        stateToPredict = batch[:, 0].reshape(self.batchSize).tolist()
+        nextStateToPredict = batch[:, 1].reshape(self.batchSize).tolist()
+
+        statePrediction = self.model.predict(np.reshape(
+            stateToPredict, (self.batchSize, 76, 384, 4)))
+        nextStatePrediction = self.model.predict(np.reshape(
+            nextStateToPredict, (self.batchSize, 76, 384, 4)))
+        statePrediction = np.array(statePrediction)
+        nextStatePrediction = np.array(nextStatePrediction)
+
+        for i in range(self.batchSize):
+            action = actions[i]
+            reward = rewards[i]
+            nextState = nextStatePrediction[i]
+            qval = statePrediction[i, action]
+            statePrediction[i, action] += alpha * (reward + 0.95 * np.max(nextState) - qval)
+            # # doubleq^
+
+        self.xTrain.append(np.reshape(
+            stateToPredict, (self.batchSize, 76, 384, 4)))
+        self.yTrain.append(statePrediction)
+        history = self.model.fit(
+            self.xTrain, self.yTrain, batch_size=5, epochs=1, verbose=0)
+        loss = history.history.get("loss")[0]
+        print("LOSS: ", loss)
+        self.xTrain = []
+        self.yTrain = []
+
+
+plotX = []
 
 if __name__ == "__main__":
+    agent = Agent() #currently agent is configured with only 2 actions
     env = Enviornment()
-    env.startGame()
-    for episode in range(1):
+    env.startGame()    
+    for i in range(4):
         state, reward, done = env.reset()
+        epReward = 0
         while not done:
+            action = agent.act(state)
+            # start_time2 = time.time()
             state, reward, done = env.step(0)
-            print(done)
-            print(reward)
-            #implement main learning loop here
+            # print("Act Time: ", time.time()-start_time2)
+
+            totalR = 0
+            for _ in range(4):  # speeds up learning if you skip frames
+                nextState, reward, done = env.step(2) #noop action
+                totalR += reward
+                if done == True:
+                    break
+            if done == True:
+                print("breaking")
+                break
+            # start_time2 = time.time()
+            agent.remember(state, nextState, action, totalR, done)
+            # print("Remember Time: ", time.time()-start_time2)
+            state = nextState
+            epReward += totalR
+
+        plotX.append(epReward)
+        print(epReward)
+        agent.learn()
+        time.sleep(1)
+
+#####
 
 
+        
+
+        
 
 #############
 #############
@@ -138,7 +271,7 @@ if __name__ == "__main__":
     
     
 
-#     screenshot = sct.grab(mon)
+#     screenshot = sct.grab(mon)                                    
 #     # img = Image.frombytes("RGB", (screenshot.width, screenshot.height), screenshot.rgb)
 #     img = np.array(screenshot)[:, :, 0]
 #     img = Image.fromarray(img)
